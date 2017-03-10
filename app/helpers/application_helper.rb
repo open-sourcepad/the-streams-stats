213d0fee80ca353effe16b395064f06bc90e4043
ruthgsp <ruthg@sourcepad.com>
2017-03-10 16:06:17 -0500

    chart

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def sec_to_hour(seconds)
+    hour = seconds/3600
+    min = (hour - hour.to_i) * 60
+    "#{hour.to_i}:#{min.to_i}"
+  end
+
+  def random_quote
+    ["Get Better Everyday",
+    "Direct with Respect",
+	  "Have Fun",
+    "Ownership",
+    "Create Value",
+    "Confident, but Humble",
+    "Max out",
+    "Be Fair and Empathetic"].sample
+  end
+
 end
--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -1,5 +1,5 @@
 class ReportsMailer < ApplicationMailer
-
+  add_template_helper(ApplicationHelper)
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -14,11 +14,66 @@ class ReportsMailer < ApplicationMailer
 
     @week_acc = ((@weekly_reports.sum(:rendered_hours)/@weekly_reports.sum(:hours_goal)) * 100).round(0)
     @month_acc = ((@monthly_reports.sum(:rendered_hours)/@monthly_reports.sum(:hours_goal)) * 100).round(0)
+    generate_graph
     hash = {
       from: "admin@sourcepad.com",
-      to: "ruthg@sourcepad.com",#user.email,
+      to: user.email,
       subject: "Daily Report"
     }
+
+    attachments.inline['header.png'] = {
+      data: File.read(Rails.root.join('app/assets/images/header.png')),
+      mime_type: 'image/png'
+    }
+
+    attachments.inline["daily_report_#{@user.name.downcase.gsub(' ','_')}.png"] = {
+      data: File.read(Rails.root.join('app', 'assets', 'images', "daily_report_#{@user.name.downcase.gsub(' ','_')}.png")),
+      mime_type: 'image/png'
+    }
+
+    attachments.inline["daily_report_pie_#{@user.name.downcase.gsub(' ','_')}.png"] = {
+      data: File.read(Rails.root.join('app', 'assets', 'images', "daily_report_pie_#{@user.name.downcase.gsub(' ','_')}.png")),
+      mime_type: 'image/png'
+    }
+
+
     mail(hash)
   end
+
+  def generate_graph
+    generate_line_graph
+    generate_pie_chart
+  end
+
+  def generate_pie_chart
+    g = Gruff::Pie.new
+    g.title = "Weekly Project Breakdown"
+    @project_data = @user.project_daily_summaries.where("work_date >= ? AND work_date <= ?", Date.yesterday.beginning_of_week, Date.yesterday).select(:project_id, "SUM(rendered_hours) as total_hours").group(:project_id)
+
+
+    @user.project_daily_summaries
+    @project_data.each_with_index do |d, index|
+      g.data(d.project.name, d.total_hours/3600)
+    end
+
+    g.write(Rails.root.join('app', 'assets', 'images', "daily_report_pie_#{@user.name.downcase.gsub(' ','_')}.png"))
+  end
+
+  def generate_line_graph
+    g = Gruff::Line.new
+    g.title = 'Daily Total Hours'
+
+    label_hash = {}
+    hours_hash = []
+
+    @weekly_reports.order(:work_date).each_with_index do |d, index|
+      label_hash.merge!(index => d.work_date.strftime("%a %m/%d"))
+      hours_hash.push(d.rendered_hours/3600)
+    end
+
+    g.labels = label_hash
+    g.data @user.name, hours_hash
+    g.write(Rails.root.join('app', 'assets', 'images', "daily_report_#{@user.name.downcase.gsub(' ','_')}.png"))
+  end
+
 end
